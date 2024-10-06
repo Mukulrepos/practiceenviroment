1340927ee14ee0d31e05971ca18bf4291b9b3238
Mukulrepos <mv282301@gmail.com>
2024-10-06 13:34:41 +0530

    identity  operators

---
+++ list of operators/identitty.py
@@ -0,0 +1,15 @@
+x = 100
+y = 200
+
+if x is y:
+    print(" x is y -> yes")
+
+else:
+    print(" x is y ->no")
+
+
+if x is not y:
+    print("x is not y -> no")
+
+else:
+    print(" x is y -> yess")
