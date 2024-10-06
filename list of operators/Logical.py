2711a019763b41c8e9857d423212bf80f1ec0741
Mukulrepos <mv282301@gmail.com>
2024-10-06 13:21:33 +0530

    hello

---
+++ list of operators/Logical.py
@@ -0,0 +1,23 @@
+# logical operator are useful to construct condition .
+# A compound condition a combination of more than one simple comndtion
+x = 100
+y = 50
+# and operator
+print(x and y)
+
+if(x>=100 and y <=60-1):
+    print("hello")
+else:
+    print("sorry i have grilfriend")
+    
+    
+if( x <=100-1 or y>=40 ):
+    print("hello everybody")
+else:
+    print("soryy i have dildo")
+    
+    
+if(not x):
+    print("poor people")
+else:
+    print("rich perople")
---
+++ list of operators/relationoperator.py
@@ -0,0 +1,42 @@
+# relation operatoer used to compare two quantitles.
+# when two operaend have one operands have a bigger number after the second number then is caleed a true other than false.
+compareone=100
+comparetwo=20
+
+# greather than opertaor
+
+x=compareone>comparetwo
+print("compare  one is 100 is greater than compare  two is (20) :-> :",x)
+
+
+
+# greather than or equal to opertaor
+
+x=compareone>=comparetwo
+print("compare  one is 100 is greater than equal to compare  two is (20) :-> ",x)
+
+
+# less than or equal to opertaor
+
+x=compareone<comparetwo
+print("compare  one is 100 is lessthan compare  two is (20) :-> ",x)
+
+
+# lessthan equal to opertaor
+
+x=compareone<=comparetwo
+print("compare  one is 100 is less than equal  compare  two is (20) :-> ",x)
+
+
+#equal equal to opertaor
+
+x=compareone==comparetwo
+print("compare  one is 100 is eqaul equal to than compare  two is (20) :-> ",x)
+
+# not equal to opertaor
+
+x=compareone!=comparetwo
+print("compare  one is 100 is not equal to compare  two is (20) :-> ",x)
+
+
+# excercise
