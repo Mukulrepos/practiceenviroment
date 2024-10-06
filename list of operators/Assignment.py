9cdcf7d6ec47757cdf6b693f05a41556d1496e66
Mukulrepos <mv282301@gmail.com>
2024-10-06 12:33:18 +0530

    operators

---
+++ list of operators/Assignment.py
@@ -0,0 +1,39 @@
+# thes operators are useful to store the right side value into a left side varibale .
+# they can also be used to perform simple arthem,etic operations like addtion and subtraction etc
+x=20
+y =5
+ 
+# Assisgment addition opertaor
+y+=x
+print("addtion :->",y)
+
+
+# Assisgment subtraction opertaor
+y-=x
+print("subtraction :->",y)
+
+
+# Assisgment Mutliplication opertaor
+y*=x
+print("Mulitplication :->",y)
+
+# Assisgment Division opertaor
+y/=x
+print("Divivon :->",y)
+
+
+# Assisgment mo0dulus opertaor
+y%=x
+print("Modulus :->",y)
+
+
+# Assisgment exponentsopertaor
+y**=x
+print("Exponents :->",y)
+
+
+# Assisgment floor opertaor
+y//=x
+print("flooor disvion is :-> ",y)
+
+
---
+++ list of operators/arthemetic_operators.py
@@ -0,0 +1,48 @@
+# thes operators are used to perform basic tasks like  athemetic addition , subtraction ,divisionetc
+
+value1 = 8
+value2 = 2
+print("Addtion is :->",value1+value2)
+
+
+# subtraction in python 
+value3=value1-value2
+print("subtraction in python : ->",value3)
+
+
+
+# multiplication 
+value4 = value1*value2
+print("multiplication is :->",value4)
+
+# division in 
+value5 = value1/value2
+print("value of divivion is : ",value5)
+
+# redainder modulus use
+value6 = value2%value2
+print("value of modulus :->",value6)
+
+
+# power value of (a**b)
+value7 =value1**value2
+print("value of expointent use is :->",value7)
+
+# floor division is 
+value8 = value1//value2
+print("floor divivion is :->",value8)
+
+
+
+x=1
+y=2
+z=3
+a=2
+b=2
+c=3
+d=(x+y)*z**a//b+c
+print(d)
+
+
+e=d*(a-b)**x+y//b+a+c
+print(e)
