b33f043f9b13bace6650b965f1684390f57e71a8
Mukulrepos <mv282301@gmail.com>
2024-10-06 18:42:27 +0530

    hello

---
+++ control statement/if_else.py
@@ -0,0 +1,20 @@
+# if else is used to when the statement of condtion is not true then the else condtion say statemet 
+
+
+# ex car start command 
+# command is not started then not started the car
+import winsound as wins
+# car = str(input("enter the command of car start and stop :-> "))
+# if car =="start" or car =="START":
+#     print("car is started ")
+# else:
+#     print("car is not started ")
+    
+    
+    
+# print("open gate ranger :->")    
+# age = int(input("enter the age when  drive ot not"))
+# if age >=20 or age <=80:
+#     wins.Beep(700,200)
+#     print("drive car")
+    
